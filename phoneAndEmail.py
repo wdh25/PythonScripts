27afc117a9b554f776d9702e6bb9de5d3e21c768
Dean Hollingworth <william.dean.hollingsworth@gmail.com>
2020-02-16 02:45:13 -0600

    Update phoneAndEmail.py

--- phoneAndEmail.py
+++ phoneAndEmail.py
@@ -2,9 +2,9 @@
 
 import re, pyperclip
 
-#TODO: Create a regex for phone numbers
+#Regex for phone numbers
 phoneRegex = re.compile(r'''
-# 415-555-000, 555-0000, (451) 555-000, 555-0000 ext 12345, ext., x12345
+# 415-555-000, 555-0000, (451) 555-000, 555-0000 ext 12345, ext., x12345, x 12345
 (
 ((\d\d\d) | (\(\d\d\d\)))?  # area code (optional)
 (\s | - | .)                # first seperator
@@ -15,7 +15,7 @@ phoneRegex = re.compile(r'''
     (\d{2:5}))?             #extension number-part optional
 )
 ''', re.VERBOSE)
-#TODO: Create a regex for email addresses
+#Regex for email addresses
 emailRegex = re.compile(r'''
 #some.+_thing@some.+_thing.com
 
@@ -24,15 +24,15 @@ emailRegex = re.compile(r'''
 [a-zA-Z0-9_.+]+             # domain name part
 
 ''', re.VERBOSE)
-#TODO: Get the text off the clipboard
+#Get the text off the clipboard
 text = pyperclip.paste()
-#TODO: Extract the email/phone number from this text
+#Extract the email/phone number from this text
 extractedPhone = phoneRegex.findall(text)
 extractedEmail = emailRegex.findall(text)
 
 allPhoneNumbers = []
 for phoneNumber in extractedPhone:
     allPhoneNumbers.append(phoneNumber[0])
-#TODO: Copy the extracted email/phone to the clipboard
+#Copy the extracted email/phone to the clipboard
 results = '\n'.join(allPhoneNumbers) + '\n' + '\n'.join(extractedEmail)
 pyperclip.copy(results)
